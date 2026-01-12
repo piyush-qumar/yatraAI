baae72bdbe61c85caa9fb3e3cd25d3f24a530ef0
Piyush Kumar <piyushkumar5271@gmail.com>
2026-01-12 19:52:03 +0530

    preparing containerized data for the llm, to prevent hallucination

--- ai-service/app/routers/ai.py
+++ ai-service/app/routers/ai.py
@@ -1,21 +1,17 @@
 from fastapi import APIRouter
 from app.schemas.destination import DestinationContext, AISummaryResponse
+from app.chains.summary_chain import summary_chain
 
 router = APIRouter()
 
 @router.post("/generate-summary", response_model=AISummaryResponse, tags=["AI"])
 def generate_summary(payload: DestinationContext):
-    #stub: to be replaced with llm later
-        summary = (
-        f"{payload.name} is a popular destination in {payload.region}. "
-        f"It is known for its {payload.terrain.lower()} terrain and is best visited during "
-        f"{', '.join(payload.bestMonths)}. Visitors can enjoy activities like "
-        f"{', '.join(payload.dos)}. However, {', '.join(payload.donts)} to ensure a pleasant experience."
-    # ).format(
-    #     name=payload.name,
-    #     best_months=", ".join(payload.bestMonths),
-    #     region=payload.region,
-    #     dos=", ".join(payload.dos),
-    #     donts=", ".join(payload.donts)
-        )
-        return {"summary": summary}
+    summary = summary_chain.invoke({
+        "name": payload.name,
+        "region": payload.region,
+        "terrain": payload.terrain,
+        "bestMonths": ", ".join(payload.bestMonths),
+        "dos": ", ".join(payload.dos),
+        "donts": payload.donts
+    })
+    return {"summary": summary}
