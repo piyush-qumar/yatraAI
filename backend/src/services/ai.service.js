8407150f9814f8db4f721ee28113f82df3734291
Piyush Kumar <piyushkumar5271@gmail.com>
2026-01-15 22:27:43 +0530

    created function to interact with external AI service created an indexdestination to send tot he endpoint for persisting in chromdb

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -1,7 +1,9 @@
 const axios = require('axios');
 const AI_SERVICE_URL = 'http://localhost:8000/api/generate-summary';
+const AI_INDEX_URL = 'http://localhost:8000/api/index-destination';
 
-const generateDestinationSummary = async(destination) => {
+
+const generateDestinationSummary = async (destination) => {
     const payload = {
         name: destination.name,
         region: destination.region,
@@ -10,10 +12,33 @@ const generateDestinationSummary = async(destination) => {
         dos: destination.dos || [],
         donts: destination.donts || [],
     };
-    const response  = await axios.post(AI_SERVICE_URL, payload);
+    const response = await axios.post(AI_SERVICE_URL, payload);
     return response.data.summary;
+};
+
+const indexDestination = async (destination) => {
+    const indexText = `
+                        Destination: ${destination.name}
+                        Region: ${destination.region}
+                        Terrain: ${destination.terrain}
+                        Best months to visit: ${destination.bestMonths.join(", ")}
+                        Safety tips: ${destination.safetyTips.join(", ")}
+                        Dos: ${destination.dos.join(", ")}
+                        Donts: ${destination.donts.join(", ")}
+                        `;
+    await axios.post(AI_INDEX_URL, {
+        id: destination._id.toString(),
+        // text: destination.aiSummary,
+        text: indexText,
+        metadata: {
+            name: destination.name,
+            region: destination.region,
+            terrain: destination.terrain,
+        }
+    });
 };
 
 module.exports = {
     generateDestinationSummary,
+    indexDestination,
 };
