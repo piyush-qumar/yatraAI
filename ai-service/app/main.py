64d332f963710d651044b3e19761ee8669dd1657
Piyush Kumar <piyushkumar5271@gmail.com>
2026-01-17 16:22:32 +0530

    cors preflight issue fix

--- ai-service/app/main.py
+++ ai-service/app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import ai, health
 
 app = FastAPI(
     title="Yatra AI Service",
     description="API service for Yatra AI functionalities",
     version="1.0.0"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # or ["*"]
+    allow_credentials=True,
+    allow_methods=["*"],   # THIS enables OPTIONS
+    allow_headers=["*"],
 )
 
 app.include_router(ai.router, prefix = "/api")
